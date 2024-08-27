96498e2166abd96d5068698bb1272da4ebc5de24
Rodry <rodrymza@gmail.com>
2024-08-26 23:32:36 -0300

    modificacion minima

--- condicional ternario.py
+++ condicional ternario.py
@@ -1,8 +1,9 @@
 verificar=False
 edad=0
 while not verificar:
-    edad=int(input("Ingresa tu edad: "))
+    edad=input("Ingresa tu edad: ")
     verificar= True if edad>=1 and edad<99 else False
     if verificar==False: print("Ingresa una edad correcta")
+    if verificar==True : edad=int(edad)
 mensaje= "Eres mayor de edad" if edad>=18 else "Eres menor de edad"
 print(mensaje)
