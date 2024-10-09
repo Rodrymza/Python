7d895863a11e498097635396f3ba0bcafcb2e28a
rodrymza <164051945+Rodrymza@users.noreply.github.com>
2024-10-09 14:11:30 -0300

    Delete Ejercicios AprendeconAlf/Datos Simples/Listas-Tuplas directory

--- Ejercicios AprendeconAlf/Datos Simples/Listas-Tuplas/Ejercicio01-03.py
+++
@@ -1,8 +0,0 @@
-materias = ["Matematica", "Fisica", "Historia", "Lengua"]
-notas = []
-for materia in materias:
-    notas.append(int(input(f"Ingresa nota de {materia}")))
-
-for materia, nota in zip(materias, notas):
-    print(f"{materia}: {nota}")
-    
